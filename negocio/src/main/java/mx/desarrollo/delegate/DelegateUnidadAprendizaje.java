6093faa99e7fc55147f2a8c959ffb6ba5bd049c1
erick <erick.gamboa@uabc.edu.mx>
2026-09-13 20:54:24 -0700

    validar los atributos y registrar la unidad de aprendizaje en la capa negocio

--- negocio/src/main/java/mx/desarrollo/delegate/DelegateUnidadAprendizaje.java
+++ negocio/src/main/java/mx/desarrollo/delegate/DelegateUnidadAprendizaje.java
@@ -2,9 +2,37 @@ package mx.desarrollo.delegate;
 
 import mx.desarrollo.entity.Unidad_aprendizaje;
 import mx.desarrollo.persistence.integration.ServiceLocator;
+import org.eclipse.persistence.jpa.jpql.tools.spi.IEntity;
 
 public class DelegateUnidadAprendizaje {
+
     public void saveUnidadAprendizaje(Unidad_aprendizaje unidad){
-        ServiceLocator.getInstanceUnidadAprendizajeDAO().save(unidad);
+
+        //usar metodo
+        if(validarUnidad(unidad) == true) {
+            ServiceLocator.getInstanceUnidadAprendizajeDAO().save(unidad);
+        }
+
+        }
+
+    //validar la unidad
+    public boolean  validarUnidad(Unidad_aprendizaje unidad){
+
+        //validar todos sus atributos
+        if(unidad.getNombre() == null || unidad.getNombre().equals("")){
+            return false;
+        }
+        if(unidad.getHorasLab()<0 || unidad.getHorasLab()>4){
+            return false;
+        }
+        if(unidad.getHorasClase()<0 || unidad.getHorasClase()>4){
+            return false;
+        }
+        if(unidad.getHorasTaller()<0 || unidad.getHorasTaller()>4){
+            return false;
+        }
+
+        return true;
     }
+
 }
